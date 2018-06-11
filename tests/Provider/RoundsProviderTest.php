e58270851178241f29a54a8d202891f6ec6edffd
Andrea Stronati <astronati@vendini.com>
2018-06-11 11:19:18 +0200

    ref #5 Fix date as W3C format

--- tests/Provider/RoundsProviderTest.php
+++ tests/Provider/RoundsProviderTest.php
@@ -17,7 +17,8 @@ class RoundsProviderTest extends TestCase
               ['num_match' => 2, 'start_time' => '2018-06-17T17:00:00.000Z'],
               ['num_match' => 17, 'start_time' => '2018-07-17T16:00:00.000Z'],
             ],
-            2
+            2,
+            '2018-06-17T16:00:00+00:00'
           ],
         ];
     }
@@ -26,10 +27,13 @@ class RoundsProviderTest extends TestCase
      * @dataProvider dataProvider
      * @param array $apiResponse
      * @param int $expectedRoundsCount
+     * @param string $expectedFirstMatchDate
      */
-    public function testProvideRounds($apiResponse, $expectedRoundsCount)
+    public function testProvideRounds($apiResponse, $expectedRoundsCount, $expectedFirstMatchDate)
     {
         $provider = new RoundsProvider(new RoundFinder());
-        $this->assertSame($expectedRoundsCount, count($provider->provideRounds($apiResponse)));
+        $rounds = $provider->provideRounds($apiResponse);
+        $this->assertSame($expectedRoundsCount, count($rounds));
+        $this->assertSame($expectedFirstMatchDate, $rounds[0]->getFirstMatchDate());
     }
 }
